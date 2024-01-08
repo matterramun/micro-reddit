dda5d143a41cbf3c735eeede7f7617868a11e47b
matterramun <Veradux4@protonmail.com>
2024-01-07 21:48:32 -0500

    Fix User assoc with comment to properly be associated.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments
+  has_many :comment
 
   validates :username, presence: true, uniqueness: true, length: { minimum:2 } 
   validates :email, presence:true, uniqueness: true, length: { minimum:4 }
