167a4196ccf6db3885989c4accabe7ed92b6faee
matterramun <Veradux4@protonmail.com>
2024-01-07 20:39:19 -0500

    Add validation to ensure user is present in post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true, length: { maximum:280 } 
+  validates :title, presence: true, length: { maximum:280 }
+  validates :user, presence: true 
 end
